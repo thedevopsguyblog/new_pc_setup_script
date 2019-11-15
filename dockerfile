b89fd6b357d8ee75c22692ac8029742c553bfb4d
Nobel W <nobelw@Nobels-MBP.AC800S>
2019-11-16 00:09:29 +1100

    added travis ci for testing

--- dockerfile
+++ dockerfile
@@ -1,4 +1,5 @@
 FROM centos:latest
 COPY /. /root/pc-setup
 WORKDIR /root/pc-setup
+RUN yum install which
 RUN [ "sh", "-c", "yum install wget -y && wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x ./jq && cp jq /usr/bin"]
--- install_apps.sh
+++ install_apps.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 applist=$(cat applist.json)
+brewApp=$(which brew)
 
-#echo "Installing all apps homebrew and yum apps under the user profile $LOGNAME (i'll have to sudo)"
+if [$brewApp == '/usr/local/bin/brew']
 
-installapps () {
-yum=$(echo $applist | jq --raw-output '.yumapplist[]')
-    for i in $yum; do
-            "installing yum app $yum"
-    done
-brew=$(echo $applist | jq '.brewapplist')
-    for i in $brew; do
-            echo "installing brew app $brew"
-    done
-brewCask=$(echo $applist | jq '.brewcaskapplist')
-    for i in $brewCask; do
-            echo "installing brew cask app $brewCask"
-    done
-}
+then
+    echo 'in the then code'
+else
+    echo 'in the else'
 
-installapps applist
+fi
+#installapps () {
+#yum=$(echo $applist | jq --raw-output '.yumapplist[]')
+#    for i in $yum; do
+#            "installing yum app $yum"
+#    done
+#brew=$(echo $applist | jq '.brewapplist')
+#    for i in $brew; do
+#            echo "installing brew app $brew"
+#    done
+#brewCask=$(echo $applist | jq '.brewcaskapplist')
+#    for i in $brewCask; do
+#            echo "installing brew cask app $brewCask"
+#    done
+#}
+
