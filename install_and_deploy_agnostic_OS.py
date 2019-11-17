d473ce228cf5f9f45c2e29bd90092adf85b9437b
Nobel W <nobelw@Nobels-MBP.AC800S>
2019-11-18 00:27:32 +1100

    switched to using python for machine builds

--- install_and_deploy_agnostic_OS.py
+++ install_and_deploy_agnostic_OS.py
@@ -1,4 +1,34 @@
-import os
+import sys
+import json
 
+class wipeAndLoadmyOS:
+    def __init__(self, machinename, getJson, defineOS):
+        self.machinename = "nobesMachine"
+        self.getJson()
+        self.defineOS()
 
-function 
+        
+    def getJson(self):
+        with open('./applist.json', 'r') as jsonData:
+            applist = json.load(jsonData)
+            self.defineOS()
+            return applist
+
+    def setNixHostname(self, machinename):
+        '''this func can be used across linux and darwin'''
+        #code to set the hostname
+
+    def defineOS(self):
+        if sys.platform == 'linux':
+            def buildLinux(self, applist):
+                print ('in the linux build')
+        elif sys.platform == 'darwin':
+            def buildMac(self, applist):
+                print ('\nin the Mac build')
+        elif sys.platform == 'windows':
+            def buildWindows(self, applist):
+                print ('in the Windows build')
+
+    
+wipeAndLoadmyOS()
+
--- install_apps.sh
+++ install_apps.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
-applist=$(cat applist.json)
-brewApp=$(which brew)
 
-if [$brewApp == '/usr/local/bin/brew']
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || yum install wget -y
 
-then
-    echo 'in the then code'
-else
-    echo 'in the else'
-
-fi
-#installapps () {
-#yum=$(echo $applist | jq --raw-output '.yumapplist[]')
-#    for i in $yum; do
-#            "installing yum app $yum"
-#    done
-#brew=$(echo $applist | jq '.brewapplist')
-#    for i in $brew; do
-#            echo "installing brew app $brew"
-#    done
-#brewCask=$(echo $applist | jq '.brewcaskapplist')
-#    for i in $brewCask; do
-#            echo "installing brew cask app $brewCask"
-#    done
-#}
-
+yum install git -y && git clone https://github.com/thedevopsguyblog/new_pc_setup_script.git || brew install git && git clone https://github.com/thedevopsguyblog/new_pc_setup_script.git
